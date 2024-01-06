5c993aed30cb6bc433c197e2a6d6b4ce429b1a1c
girihanbudi <Ghanbudi@gmail.com>
2024-01-07 02:40:32 +0700

    fixed token behavior

--- src/app/authorization.service.ts
+++ src/app/authorization.service.ts
@@ -1,20 +1,26 @@
 import { Injectable, inject } from '@angular/core';
 import Admin from '../types/admin';
-import { CookieService } from 'ngx-cookie-service';
-import { v4 as uuidv4 } from 'uuid';
+import { BehaviorSubject } from 'rxjs';
+
+const accessTokenKey = '_Secure.at';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorizationService {
-  cookieService = inject(CookieService);
+  identityValue = new BehaviorSubject(this.identity);
 
-  createAccessToken() {
-    const expires = new Date();
-    this.cookieService.set(
-      '_Secure.at',
-      uuidv4(),
-      expires.setDate(expires.getDay() + 30)
-    );
+  set identity(admin: Admin | undefined) {
+    this.identityValue.next(admin); // this will make sure to tell every subscriber about the change.
+    localStorage.setItem(accessTokenKey, JSON.stringify(admin));
+  }
+
+  get identity() {
+    const data = localStorage.getItem(accessTokenKey);
+    if (!data) return undefined;
+    else {
+      if (data === 'undefined') return undefined;
+      return JSON.parse(data) as Admin;
+    }
   }
 }
