01a4c77a30e24e19713a32da43d4a1922bc4bce6
girihanbudi <Ghanbudi@gmail.com>
2024-01-06 18:10:59 +0700

    add mock data

---
+++ src/mock/mock-admins.ts
@@ -0,0 +1,13 @@
+import Admin from '../types/admin';
+
+export const adminList: Admin[] = [
+  {
+    id: 1,
+    firstName: 'Giri',
+    lastName: 'Hanbudi',
+    email: 'giri-hanbudi@mailinator.com',
+    password: '123456',
+  },
+];
+
+export default adminList;
